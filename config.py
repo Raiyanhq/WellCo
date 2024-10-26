92e85ffa167b89547294b21cabc68089c75d200f
Raiyan Haque <129631894+Raiyanhq@users.noreply.github.com>
2024-10-26 18:57:12 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 # config.py
-GEMINI_API_KEY = "AIzaSyCuF_GnsVfX3zruhHJ0yciAFkgsP_-qoiY"
+GEMINI_API_KEY = "OUR_API_KEY"
 GOOGLE_APPLICATION_CREDENTIALS = "/Users/raiyanhaque/Desktop/WellCo/wellco-439817-a255239308fd.json"
