2b0659497875cb07ffa938ebd8cd862b7166fd90
Raiyan Haque <129631894+Raiyanhq@users.noreply.github.com>
2024-10-27 04:45:46 -0400

    test_google_cloud.js

---
+++ test_google_cloud.js
@@ -0,0 +1,5 @@
+// Importing Google Cloud libraries
+const speech = require('@google-cloud/speech');
+const textToSpeech = require('@google-cloud/text-to-speech');
+
+console.log("Google Cloud libraries imported successfully!");
--- test_google_cloud.py
+++
@@ -1,5 +0,0 @@
-# test_google_cloud.py
-from google.cloud import speech
-from google.cloud import texttospeech
-
-print("Google Cloud libraries imported successfully!")
